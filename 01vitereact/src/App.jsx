17e3c56d7cf618d039374fdf338c2336d99341de
Harshit Satya <harshitsatya5@gmail.com>
2024-04-01 16:11:33 +0530

    some changes

--- 01vitereact/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Chai aur React: Vite App</h1>
-    </>
-  );
-}
-
-export default App;
--- 01vitereact/src/MyApp.jsx
+++
@@ -1,9 +0,0 @@
-const MyApp = () => {
-  return (
-    <div>
-      <h1>Hello from MyApp</h1>
-    </div>
-  );
-};
-
-export default MyApp;
