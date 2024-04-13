f7eb63acdbc1529ea10948b997b471fc327593ef
Harshit Satya <harshitsatya5@gmail.com>
2024-04-13 18:00:14 +0530

    basics of context-api

---
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import UserContext from "./UserContext";
+
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = React.useState("");
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
